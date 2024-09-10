7ddcfbbc3d79242a5a972b225af15f1910749220
nostromo76 <ivans1976@gmail.com>
2024-09-10 19:10:49 +0200

    2nd commt routs

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/test', function () {
+    return 'test';
+});
+Route::post('/', function (Request $request) {
+    return dd($request->all());
+});
+
+Route::put('/', function (Request $request) {
+    return dd($request->all());
 });
